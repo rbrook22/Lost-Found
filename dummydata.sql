1ec9f112d7bdd258800af219d2fbbdee8a1a8140
Richard Brooks <rickbrooks22@gmail.com>
2018-06-28 15:03:35 -0400

    created dummy data file

---
+++ dummydata.sql
@@ -0,0 +1,7 @@
+INSERT INTO foundFormData
+    (Title, ItemDescription, DateFound, TimeFound, LocationFound, ImageURL, Email)
+VALUES 
+    ('IphoneX', 'Iphone 10 Black 256GB', '06/28/2018', '1 PM', '9050 Riverbend Manor, Alpharetta, GA, 30022', 'imgurl.com', 'rickbroks22@gmail.com'),
+    ('Wallet', 'Brown leather gucci wallet', '05/01/2017', '12 pm', '3423 Piedmont rd NE, Atlanta, GA, 30324', 'jiggywithit.com', 'jimmy@gmail.com'), 
+    ('Keys', 'audi keys with 3 silver keys', '01/20/2018', '11pm', 'Rivermont Club, Santa Clara, CA, 00000', 'craziness.com', 'johnny@johnnyjohn.com')
+    ;
