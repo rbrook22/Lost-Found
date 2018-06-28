094978d4ec2bdb15191c333fe40c4d357c5816cd
Richard Brooks <rickbrooks22@gmail.com>
2018-06-28 17:16:53 -0400

    aded functions in db.js file for queries selecting all, by title, and userid

--- db.js
+++ db.js
@@ -7,3 +7,44 @@ const cn = {
     password: ''
 };
 const db = pgp(cn);
+
+function getAll() {
+    return db.any('Select * from foundformdata');
+}
+
+// getAll()
+//     .then((data) => { console.log(data); })
+//     .catch((error) => { console.log(error); });
+
+
+function getOne(userid) {
+    return db.oneOrNone('Select * from foundformdata where userid=$1', [userid]);
+}
+
+// getOne(2)
+//     .then(function(data) {
+//         // success;
+//         console.log(data);
+//     })
+//     .catch(function(error) {
+//         // error;
+//         console.log('this is the error that happened');
+//         console.log(error);
+//     });
+
+function searchByTitle(searchString) {
+    return db.any("select * from foundformdata where title ilike '%$1#%'", [searchString]);
+}
+
+searchByTitle('00000')
+    .then((data) => { console.log(data); })
+    .catch((error) => { console.log(error); });
+
+
+
+    module.exports = {
+        getAll, 
+        getOne,
+        searchByTitle
+
+    };
