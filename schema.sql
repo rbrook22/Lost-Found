a938e7ed621d6ee2e5e2a785f5ec2b2c9d4e3066
Richard Brooks <rickbrooks22@gmail.com>
2018-06-28 14:50:09 -0400

    created a schema sql file for creating table

---
+++ schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE foundFormData (
+    Userid serial primary key,
+    Title varchar(300),
+    ItemDescription text, 
+    DateFound varchar(300),
+    TimeFound varchar(300),
+    LocationFound varchar(300),
+    ImageURL varchar(300),
+    Email varchar(300)
+);
