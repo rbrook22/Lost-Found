e68452960e0acd324a61e22dc65c7d921fe22c70
Richard Brooks <rickbrooks22@gmail.com>
2018-07-10 13:37:39 -0400

    added search by title function

--- index.js
+++ index.js
@@ -10,17 +10,41 @@ app.use(bodyParser.urlencoded({ extended: false }));
 const static = express.static;
 app.use(static('public'));
 
+// Loads Home Page
 app.get('/', (req, res) => {
     res.render('homepage');
 });
 
-app.get('/foundItem', (req, res, next) => {
+// Loads Found Item Page when Found Button Clicked and loads all found items
+app.get('/lostItem', (req, res, next) => {
     foundItem.getAll()
     .then((data) => {   
-        res.render('foundItem', {items:data});
+        res.render('lostItem', {items:data});
     })
     .catch((error) => { console.log(error); });
 });
+
+// Need to change below to search filters not posting data
+
+// app.post('/lostItem', (req,res) => {
+//     // res.send("You submitted the form");
+//     console.log(req.body);
+//     console.log(req.body.title);
+//     console.log(req.body.description);
+//     console.log(req.body.date);
+//     console.log(req.body.time);
+//     console.log(req.body.location);
+//     console.log(req.body.email);
+
+//     foundItem.addItem(req.body.title, req.body.description, req.body.date, req.body.time, req.body.location, req.body.email)
+//     .then((data) => {
+//         res.redirect('/lostItem');
+//     })
+// })
+app.get('/foundItem', (req, res, next) => { 
+        res.render('foundItem');
+    })
+
 app.post('/foundItem', (req,res) => {
     // res.send("You submitted the form");
     console.log(req.body);
@@ -33,10 +57,10 @@ app.post('/foundItem', (req,res) => {
 
     foundItem.addItem(req.body.title, req.body.description, req.body.date, req.body.time, req.body.location, req.body.email)
     .then((data) => {
-        res.redirect('/foundItem');
+        res.render('foundItem');
     })
 })
-
+// Loads Edit page to update item by ID
 app.get('/:itemid/editpage', (req, res) => {
     foundItem.getOne(req.params.itemid)
     .then((data) => {
@@ -56,16 +80,51 @@ app.post('/:itemid/editpage', (req, res) => {
     console.log(req.body.email);
     
     foundItem.updateById(
-         req.body.title,
-          req.body.description,
-           req.body.date,
-            req.body.time,
-            req.body.location,
-             req.body.email,
-             req.params.itemid)
+        req.body.title,
+        req.body.description,
+        req.body.date,
+        req.body.time,
+        req.body.location,
+        req.body.email,
+        req.params.itemid)
     .then((data) => {
         console.log(data);
-        res.redirect('/foundItem');
+        res.redirect('/lostItem');
+    })
+})
+
+// Delete Item by ID
+app.get('/:itemid/deletepage', (req, res) => {
+    foundItem.getOne(req.params.itemid)
+    .then((data) => {
+        console.log(data);
+        res.render('deletepage', data)
+    })
+})
+app.post('/:itemid/deletepage', (req, res) => {
+    console.log(req.body);
+    console.log(req.body.title);
+    console.log(req.body.description);
+    console.log(req.body.date);
+    console.log(req.body.time);
+    console.log(req.body.location);
+    console.log(req.body.email);
+
+    foundItem.deleteById(req.params.itemid)
+    .then((data) => {
+        console.log(data);
+        res.redirect('/lostItem');
+    })
+})
+
+// Search By Title
+app.post('/searchpage', (req, res) => {
+    console.log(req.body.title);
+
+    foundItem.searchByTitle(req.body.title)
+    .then((data) =>{
+        console.log(data);
+        res.render('searchpage', {items:data});
     })
 })
 
