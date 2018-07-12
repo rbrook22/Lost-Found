f2b49ad993a0423f0003668ef0e438656d7ee4bf
Richard Brooks <rickbrooks22@gmail.com>
2018-07-12 14:54:29 -0400

    updated styling

--- schema.sql
+++ schema.sql
@@ -1,7 +1,7 @@
 CREATE TABLE foundFormData (
     itemId serial primary key,
     Title varchar(300),
-    ItemDescription text, 
+    ItemDescription varchar(500), 
     DateFound varchar(300),
     TimeFound varchar(300),
     LocationFound varchar(300),
